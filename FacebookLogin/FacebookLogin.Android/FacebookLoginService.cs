0da97a44c609a1644e142d7c54a4bb57944bc75a
TuanNV2 <tuan.nguyen.van2@ntq-solution.com.vn>
2020-04-28 17:43:54 +0700

    init

---
+++ FacebookLogin/FacebookLogin.Android/FacebookLoginService.cs
@@ -0,0 +1,53 @@
+﻿using Android.Content;
+using Android.Views;
+using FacebookLogin.Droid;
+using System;
+using Xamarin.Facebook;
+using Xamarin.Facebook.Login;
+using Xamarin.Facebook.Login.Widget;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: Dependency(typeof(FacebookLoginService))]
+namespace FacebookLogin.Droid
+{
+    public class FacebookLoginService : Java.Lang.Object, IFacebookLoginService , ICallbackManager, IFacebookCallback
+    {
+        private LoginButton loginButton;
+        private ICallbackManager mCallbackManager;
+        private IFacebookCallback facebookCallback;
+
+        public FacebookLoginService()
+        {
+            loginButton = new LoginButton(Android.App.Application.Context);
+            loginButton.RegisterCallback(this, this);
+
+
+        }
+
+        public void InitButton(StackLayout stackLayout)
+        {
+            stackLayout.Children.Add(loginButton);
+        }
+
+        public bool OnActivityResult(int requestCode, int resultCode, Intent data)
+        {
+            return true;
+        }
+
+        public void OnCancel()
+        {
+            
+        }
+
+        public void OnError(FacebookException error)
+        {
+            
+        }
+
+        public void OnSuccess(Java.Lang.Object result)
+        {
+           
+        }
+    }
+}
---
+++ FacebookLogin/FacebookLogin.iOS/FacebookLoginService.cs
@@ -0,0 +1,36 @@
+﻿using Facebook.LoginKit;
+using FacebookLogin.iOS;
+using Foundation;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: Dependency(typeof(FacebookLoginService))]
+namespace FacebookLogin.iOS
+{
+    public class FacebookLoginService : NSObject, ILoginButtonDelegate , IFacebookLoginService
+    {
+        private LoginButton loginButton;
+        public FacebookLoginService()
+        {
+            loginButton = new LoginButton();
+            loginButton.Delegate = this;
+        }
+
+
+        public void DidComplete(LoginButton loginButton, LoginManagerLoginResult result, NSError error)
+        {
+            
+        }
+
+        public void DidLogOut(LoginButton loginButton)
+        {
+            
+        }
+
+        public void InitButton(StackLayout stackLayout)
+        {
+            stackLayout.Children.Add(loginButton);
+        }
+    }
+}
